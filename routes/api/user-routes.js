c416b804396678eeacd35b2bf3379f17d956dbeb
katredford <katharine.redford@gmail.com>
2022-07-09 22:10:30 -0500

    password protection

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -3,8 +3,11 @@ const { User } = require('../../models');
 
 // GET /api/users
 router.get('/', (req, res) => {
+  
   // Access our User model and run .findAll() method)
-  User.findAll()
+  User.findAll({
+    attributes: { exclude: ['password'] }
+})
     .then(dbUserData => res.json(dbUserData))
     .catch(err => {
       console.log(err);
@@ -15,6 +18,7 @@ router.get('/', (req, res) => {
 // GET /api/users/1
 router.get('/:id', (req, res) => {
   User.findOne({
+    attributes: { exclude: ['password'] },
     where: {
       id: req.params.id
     }
@@ -53,6 +57,7 @@ router.put('/:id', (req, res) => {
 
   // if req.body has exact key/value pairs to match the model, you can just use `req.body` instead
   User.update(req.body, {
+    individualHooks: true,
     where: {
       id: req.params.id
     }
